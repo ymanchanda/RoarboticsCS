7d6c64ba131a034f391329db76aa0868b456bf2f
adel <adel.jumaily@gmail.com>
2024-11-18 20:50:29 -0500

    11/18/2024 8:50 pm Added claw state machine and subsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/subsystems/ITDClawSubsystem.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.team.subsystems;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.lib.drivers.RevServo;
+import org.firstinspires.ftc.teamcode.team.states.DroneStateMachine;
+
+public class ITDClawSubsystem implements ISubsystem<DroneStateMachine, DroneStateMachine.State> {
+    private static DroneStateMachine clawStateMachine;
+    private RevServo ClawServo;
+
+
+    public ITDClawSubsystem(RevServo clawServo){
+        setClawStateMachine(new DroneStateMachine());
+        setGripperServo(clawServo);
+    }
+
+    @Override
+    public DroneStateMachine getStateMachine() {
+        return clawStateMachine;
+    }
+
+    @Override
+    public DroneStateMachine.State getState() {
+        return getStateMachine().getState();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Gripper Subsystem";
+    }
+
+    @Override
+    public void writeToTelemetry(Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void update(double dt) {
+        getStateMachine().update(dt);
+        getClawServo().setPosition(getState().getPosition());
+    }
+
+    public static void setClawStateMachine(DroneStateMachine gripperStateMachine) {
+        ITDClawSubsystem.clawStateMachine = gripperStateMachine;
+    }
+
+    public RevServo getClawServo() {
+        return ClawServo;
+    }
+
+
+    public void setGripperServo(RevServo clawServo) {
+        this.ClawServo = clawServo;
+    }
+}
